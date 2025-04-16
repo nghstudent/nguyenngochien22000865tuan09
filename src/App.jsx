893c135d83c9061a688ea95769328842476a391e
H82M19\Student <Student>
2025-04-16 16:46:44 +0700

    them minh chung test Counter 3 phuong thuc

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,21 @@
-// import './App.css'
-// import UseReducer from './components/CounterReducer'
-// import { Provider } from 'react-redux';
-// import Store from './components/UseRedux/Store';
-// import CounterRedux from './components/UseRedux/CounterRedux';
-// import store from './components/UseReduxToolkit/store';
-// import CounterReduxToolkit from './components/UseReduxToolkit/Counter';
-
-// function App() {
-
-//   return (
-//     <>
-//       <h1>Test DEMO</h1>
-//       <UseReducer />
-//       <hr />
-//       <Provider store={Store}>
-//         <CounterRedux />
-//       </Provider>
-//       <hr />
-//       <Provider store={store}>
-//         <CounterReduxToolkit />
-//       </Provider>
-//     </>
-//   )
-// }
-
-// export default App
-
 import './App.css';
-import UseReducer from './components/CounterReducer'; // Component sử dụng useReducer
+import UseReducer from './components/CounterReducer';
 import { Provider } from 'react-redux';
-import Store from './components/UseRedux/Store'; // Redux store truyền cho CounterRedux
-import CounterRedux from './components/UseRedux/CounterRedux'; // Component sử dụng Redux
-import store from './components/UseReduxToolkit/store'; // Redux store cho Redux Toolkit
-import CounterReduxToolkit from './components/UseReduxToolkit/Counter'; // Component sử dụng Redux Toolkit
+import Store from './components/UseRedux/Store';
+import CounterRedux from './components/UseRedux/CounterRedux';
+import store from './components/UseReduxToolkit/store'; 
+import CounterReduxToolkit from './components/UseReduxToolkit/Counter';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="demo-title">Test DEMO</h1>
+      <h1 className="demo-title">TEST COUNTER</h1>
 
       {/* Section for UseReducer */}
       <section className="section">
         <h2>UseReducer Example</h2>
         <UseReducer />
-      </section>
+      </section>                        
       
       <hr />
 
