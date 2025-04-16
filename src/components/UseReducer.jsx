720226f2ccfdae71d0efbcb377bdd804c6f3619d
H82M19\Student <Student>
2025-04-16 16:14:43 +0700

    1. useReducer + -

---
+++ src/components/UseReducer.jsx
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+function UseReducer() {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+
+  return (
+    <div>
+      <h1>Counter Reducer: {state.count}</h1>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
+  );
+}
+
+export default UseReducer;
